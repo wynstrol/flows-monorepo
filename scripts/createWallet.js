d17b1536052693d8952d040ba22ef2b469cc75a5
atomicdomb <atomicdomb@gmail.com>
2021-10-11 13:13:39 +0200

    Docker build settings updated
    Docker build settings updated to use the latest Raiden release candidate v3.0.0rc6.

--- scripts/createWallet.js
+++ scripts/createWallet.js
@@ -15,7 +15,7 @@ const updateEnvFile = (key, value) => {
     fs.writeFileSync(
       envfilePath,
       `ETH_RPC_ENDPIONT=""`,
-      `RAIDEN_NODE_VERSION=raidennetwork/raiden:latest`
+      `RAIDEN_NODE_VERSION=docker.io/library/raiden`
     );
   }
 
@@ -26,7 +26,7 @@ const updateEnvFile = (key, value) => {
 };
 
 const provider = new ethers.providers.JsonRpcProvider(
-  "https://rpc.goerli.mudit.blog"
+  "https://goerli.infura.io/v3/960f7319ef294e5b8b151aacdc3b390c"
   //"${ETH_RPC_ENDPIONT}"
 );
 
